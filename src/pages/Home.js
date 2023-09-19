ae4b62c2f2c5332b153579f3d580714732facbda
Ben Sithisane <bsithisane@gmail.com>
2023-09-19 10:55:30 -0500

    added functionality products on homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import React, {useContext} from 'react';
-// import product context
 import { ProductContext } from '../contexts/ProductContext'
 import Product from '../components/Product'
 
@@ -20,7 +19,7 @@ const Home = () => {
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grids-cols-5 gap-[30px] 
          mx-auto md:max-w-none md:mx-0'>
           {filteredProducts.map(product => {
-            return <Product key={product.id} />
+            return <Product product={product} key={product.id} />
           })}
         </div>
       </div>
