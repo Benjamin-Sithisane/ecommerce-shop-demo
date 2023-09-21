b49522f158d007b9fe128dfda583200cefa2c306
Ben Sithisane <bsithisane@gmail.com>
2023-09-21 11:33:41 -0500

    added a significant portion of functionality to ui

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
 import React, { useContext } from 'react';
 import { SidebarContext } from '../contexts/SidebarContext';
+import { BsCart4 } from 'react-icons/bs'
 
 const Header = () => {
   const {isOpen, setIsOpen} = useContext(SidebarContext)
   return (
-    <div>
+    <header className='bg-blue-200 position-absolute'>
       <div>Header</div>
-      <div onClick={()=> setIsOpen(!isOpen)}><button className='hover-6ba8f2'>open/close sidebar</button></div>
-    </div>
+      <div onClick={()=> setIsOpen(!isOpen)}>
+          <div className='hover-6ba8f2 cursor-pointer'>
+            <BsCart4  className='text-2x1'/>
+          </div>
+        </div>
+    </header>
   )
 };
 
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,22 +2,34 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom'
 import { IoMdArrowForward } from 'react-icons/io'
 import { FiTrash2 } from 'react-icons/fi'
-//import cart item
 import CartItem from '../components/CartItem'
-//import sidebar context
 import { SidebarContext } from '../contexts/SidebarContext';
+import { CartContext } from '../contexts/CartContext';
 
 const Sidebar = () => {
   const { isOpen, handleClose } = useContext(SidebarContext)
+  console.log(useContext(CartContext))
   return (
     <div 
       className={`${
-        isOpen ? 'right-0' : 'right-full'
+        isOpen ? 'right-0' : '-right-full'
       } w-full bg-white fixed top-0 h-full 
       shadow-2x1 md:w-[35vw] xl:max-w-[30vw] transition-all 
-      duration-300 z-20 px-4 lg:px-[35px]`}
+      duration-300 z-20 px-4 lg:px-[35px] drop-shadow-xl`}
     >
-      Sidebar
+      <div className='flex items-center justify-between py-6
+      border-b'>
+        <div className='uppercase text-sm 
+        font-semibold'>Cart (0)</div>
+
+        <div
+          onClick={handleClose} 
+          className='cursor-pointer w-8 h-8 flex
+          justify-center items-center'
+        >
+          <IoMdArrowForward className='text-2x1'/>
+        </div>
+      </div>
     </div>
     )
 };
